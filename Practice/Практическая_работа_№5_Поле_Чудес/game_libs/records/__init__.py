b54c141ee295eb0561f6bde3a249f171268b2ef0
Ep1cMax <138321576+Ep1cMax@users.noreply.github.com>
2024-12-26 15:20:57 +0300

    Pract5 commit

---
+++ Practice/Практическая_работа_№5_Поле_Чудес/game_libs/records/__init__.py
@@ -0,0 +1,2 @@
+from .load_lead import load_leading_record
+from .track import track_record
---
+++ Practice/Практическая_работа_№5_Поле_Чудес/game_libs/records/load_lead.py
@@ -0,0 +1,9 @@
+def load_leading_record():
+    """
+    Загружает высший рекорд из файла records.txt
+    :return:
+    """
+    file = open('game_libs\\records.txt', 'r')
+    top = file.readline()
+    if top != '':
+        print('Самый высокий рекорд:', top)
